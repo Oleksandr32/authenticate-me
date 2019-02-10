80bf2388d6c5dd533671146b7a3f4fc99ec3594e
Oleksandr32 <lysunolexandr@ukr.net>
2019-02-10 23:26:06 +0200

    add Record model and implement CRUD functions

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -4,17 +4,26 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 import 'package:authenticate_me/model/User.dart';
+import 'package:authenticate_me/model/Record.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper._internal();
 
   factory DatabaseHelper() => _instance;
 
-  static final tableName = 'users';
-  static final idColumnName = 'id';
+  // users TABLE
+  static final userTableName = 'users';
+  static final userIdColumnName = 'id';
   static final usernameColumnName = 'username';
   static final emailColumnName = 'email';
   static final paswordColumnName = 'password';
+
+  // audit record TABLE
+  static final recordTableName = 'records';
+  static final recordIdColumnName = 'id';
+  static final userColumnName = 'user';
+  static final actionColumnName = 'action';
+  static final dateColumnName = 'date';
 
   static Database _db;
   // super user that has more feature
@@ -35,41 +44,76 @@ class DatabaseHelper {
     String path = join(databasesPath, 'users.db');
     var db = await openDatabase(path, version: 1, onCreate: _onCreate);
   
-    await this.saveUser(_admin);
+    //await this.saveUser(_admin);
 
     return db;
   }
 
   void _onCreate(Database db, int newVersion) async {
     await db.execute(
-      'CREATE TABLE $tableName('
-        + '$idColumnName INTEGER PRIMARY KEY,' 
-        + ' $usernameColumnName TEXT,' 
-        + ' $emailColumnName TEXT,'
-        + ' $paswordColumnName TEXT)'
+      'CREATE TABLE $userTableName($userIdColumnName INTEGER PRIMARY KEY, $usernameColumnName TEXT, $emailColumnName TEXT, $paswordColumnName TEXT)'
+    );
+    await db.execute(
+      'CREATE TABLE $recordTableName($recordIdColumnName INTEGER PRIMARY KEY, $userColumnName TEXT, $actionColumnName TEXT, $dateColumnName TEXT)'
     );
   }
 
   Future<int> saveUser(User user) async {
     var dbClient = await db;
-    var result = await dbClient.insert(tableName, user.toMap());
+    var result = await dbClient.insert(userTableName, user.toMap());
+    saveRecord(user, 'Register');
+
     return result;
   }
 
   Future<User> getUser(String email) async {
     var dbClient = await db;
     List<Map> result = await dbClient.query(
-      tableName,
-      columns: [idColumnName, usernameColumnName, emailColumnName, paswordColumnName],
-      where: '$email = ?',
+      userTableName,
+      columns: [userIdColumnName, usernameColumnName, emailColumnName, paswordColumnName],
+      where: '$emailColumnName = ?',
       whereArgs: [email]
     );
 
     if (result.isEmpty) {
+      saveRecord(User('unknown', email, 'unknown'), 'Try to Log in. Incorrect email.');
       return null;
     }
+    
+    var user = User.fromMap(result.first);
+    saveRecord(user, 'Log in');
 
-    return User.fromMap(result.first);
+    return user;
+  }
+
+  Future<List<User>> getAllUsers() async {
+    var dbClient = await db;
+    var result = await dbClient.query(
+      userTableName, 
+      columns: [userIdColumnName, usernameColumnName, emailColumnName, paswordColumnName]
+    );
+
+    return result.map((f) => User.fromMap(f)).toList();
+  }
+
+  Future<void> saveRecord(User user, String action) async {
+    var dbClient = await db;
+    var record = Record(
+      user.toString(),
+      action,
+      DateTime.now().toString()
+    );
+    await dbClient.insert(recordTableName, record.toMap());
+  }
+
+  Future<List<Record>> getAllRecords() async {
+    var dbClient = await db;
+    var result = await dbClient.query(
+      recordTableName,
+      columns: [recordIdColumnName, userColumnName, actionColumnName, dateColumnName]
+    );
+
+    return result.map((f) => Record.fromMap(f)).toList();
   }
 
   Future close() async {
